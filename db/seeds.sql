989411b610e57a0ca3dfca9875dffbd241cdb5c5
Rhonda Kremer <rhondakremer@gmail.com>
2019-09-16 18:29:06 -0400

    updated .sql files with code to create tables and insert info

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Don Deo prosciutto-wrapped Mediterranean burger');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Lokal Fiddy-Fiddy burger');
+
+INSERT INTO quotes (quote, author) VALUES ('Cheese-stuffed bacon-wrapped burger Szilard made that one time'');
