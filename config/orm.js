540aef335d2f5dbcf947daf2719b96dc0bd88e23
Rhonda Kremer <rhondakremer@gmail.com>
2019-09-18 16:26:03 -0400

    main page shows info from database

--- config/orm.js
+++ config/orm.js
@@ -1,26 +1,52 @@
 var connection = require("./connection.js");
 
+// Helper function for generating MySQL syntax
+/*function printQuestionMarks(num) {
+	var arr = [];
+
+	for (var i = 0; i < num; i++) {
+		arr.push("?");
+	}
+
+	return arr.toString();
+}
+
+// Helper function for generating My SQL syntax
+function objToSql(ob) {
+	var arr = [];
+
+	for (var key in ob) {
+		arr.push(key + "=" + ob[key]);
+	}
+
+	return arr.toString();
+} */
+
+
+
 var orm = {
-  selectAll: function(tableName) {
-    var queryString = "SELECT * FROM ??";
+  selectAll: function(tableName, callback) {
+    var queryString = "SELECT * FROM ??;";
     connection.query(queryString, [tableName], function(err, result) {
       if (err) throw err;
       console.log(result);
+      callback(result)
     });
   },
 
-  insertOne: function(tableName, column1, column2, column1Name) {
+    /*
+  insertOne: function(tableName, column1, column2, column1Value) {
     var queryString = "INSERT INTO ?? (??, ??) VALUES (?, false);";
 
     console.log(queryString);
 
-    connection.query(queryString, [tableName, column1, column2, column1Name], function(err, result) {
+    connection.query(queryString, [tableName, column1, column2, column1Value], function(err, result) {
       if (err) throw err;
       console.log(result);
     });
   },
 
-  /*updateOne: function(whatToSelect, tableOne, tableTwo, onTableOneCol, onTableTwoCol) {
+updateOne: function(whatToSelect, tableOne, tableTwo, onTableOneCol, onTableTwoCol) {
     var queryString = 
 
     console.log(queryString);
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,22 +2,23 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
+// Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.all(function(data) {
+  burger.selectAll(function(data) {
     var hbsObject = {
-      burgger: data
+      burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
+/*
 router.post("/api/burgers", function(req, res) {
-  burger.create([
+  burger.insertOne([
     "name", "devoured"
   ], [
     req.body.name, req.body.devoured
@@ -42,7 +43,7 @@ router.put("/api/burgers/:id", function(req, res) {
       res.status(200).end();
     }
   });
-});
+});*/
 
 
 // Export routes for server.js to use.
