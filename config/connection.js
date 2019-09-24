0cdacfb852263593bb92a9a9f885d01bb0ecf81c
Rhonda Kremer <rhondakremer@gmail.com>
2019-09-23 20:40:14 -0400

    added start script

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,7 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
+  console.log(" Using environment....");
   connection = mysql.createConnection(process.env.JAWSBD_URL);
 } else {
   connection = mysql.createConnection({
