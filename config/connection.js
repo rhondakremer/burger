f273d62659ca88b4a4bc0555efca6f85a7d46d58
Rhonda Kremer <rhondakremer@gmail.com>
2019-09-23 21:34:10 -0400

    Connection

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,14 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  console.log(" Using environment....");
-  connection = mysql.createConnection(process.env.JAWSBD_URL);
+  console.log(" Using environment:", process.env.JAWSBD_URL);
+  try {
+    connection = mysql.createConnection(process.env.JAWSBD_URL);
+  }
+  catch (error) {
+    console.log(error);
+  }
+
 } else {
   connection = mysql.createConnection({
     host: "localhost",
@@ -17,12 +23,12 @@ if (process.env.JAWSDB_URL) {
 
 
 
-  connection.connect(function (err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
-  module.exports = connection;
+module.exports = connection;
